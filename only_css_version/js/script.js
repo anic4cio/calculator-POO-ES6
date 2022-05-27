940f76795cc3fb86c9f2febdc30f0e27763f46ed
anic4cio <anicaciovinicius36@gmail.com>
2022-05-27 17:27:16 -0300

    [update] - calculator buttons logic created

--- only_css_version/js/script.js
+++ only_css_version/js/script.js
@@ -1,1 +1,46 @@
-console.log('all ready')
+class Calculator {
+    constructor() {
+        this.upperValue = document.getElementById('upper-number');
+        this.resultValue = document.getElementById('result-number');
+        this.reset = 0;
+    }
+
+    checkLastDigit(input, upperValue, reg) {
+        if((
+            !reg.test(input) &&
+            !reg.test(upperValue.substr(upperValue.length - 1))
+        )) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    btnPress() {
+        let input = this.textContent;
+        let upperValue = calc.upperValue.textContent;
+        var reg = new RegExp('^\\d+$'); // input is numbers only?
+
+        if(calc.checkLastDigit(input, upperValue, reg)) {
+            return false;
+        }
+
+        if(upperValue == '0') {
+            calc.upperValue.textContent = input;
+        } else {
+            calc.upperValue.textContent += input;
+        }
+    }
+}
+
+// new instance of object from Calculator class
+let calc = new Calculator;
+
+// all buttons into variable 'buttons'
+let buttons = document.getElementsByClassName('btn');
+
+// map all buttons
+for(let i = 0; buttons.length > i; i++) {
+    buttons[i].addEventListener('click', calc.btnPress);
+
+}
